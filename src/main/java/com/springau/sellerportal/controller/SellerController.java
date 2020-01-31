2cd58aa9df1df71873a295d08cb8472435667b13
RohitGonsalves1999 <32949288+RohitGonsalves1999@users.noreply.github.com>
2020-01-31 16:29:13 +0530

    Dummy Login

--- src/main/java/com/springau/sellerportal/controller/SellerController.java
+++ src/main/java/com/springau/sellerportal/controller/SellerController.java
@@ -1,5 +1,6 @@
 package com.springau.sellerportal.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class SellerController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String getResponse() {
 		return "Hello";
+	}
+	
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	public Boolean loginSeller(Model model) {
+		return true;
 	}
 
 }

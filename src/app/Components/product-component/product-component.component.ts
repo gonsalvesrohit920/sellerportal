41fa1e601b5b42d33c9743b58c017ea0d8058858
sahil <sahil.mohammed@accoliteindia.com>
2020-02-03 21:14:04 +0530

    =changes in pruduct sahil

--- src/app/Components/product-component/product-component.component.ts
+++ src/app/Components/product-component/product-component.component.ts
@@ -3,6 +3,7 @@ import {CookieService} from 'ngx-cookie-service';
 
 import { Router } from '@angular/router';
 import { SellerDataService } from 'src/app/providers/seller-data-service/seller-data.service';
+import { SellerServiceService } from 'src/app/providers/seller-service.service';
 
 
 @Component({
@@ -12,16 +13,19 @@ import { SellerDataService } from 'src/app/providers/seller-data-service/seller-
 })
 export class ProductComponentComponent implements OnInit {
   username: string;
-  constructor(private cookieservice: CookieService, private sellerDataService:SellerDataService , private router: Router) { }
+  constructor(private cookieservice: CookieService, private sellerDataService:SellerDataService , private router: Router,private sellerservice:SellerServiceService) { }
 
   sellerData: object;
-
+  applicationStatus1 :any
   applicationStatus = this.cookieservice.get('applicationStatus');
 
   ngOnInit() {
     this.username = this.cookieservice.get('email');
     this.sellerDataService.currentData.subscribe(sellerdata => this.sellerData = sellerdata);
-    }
+    this.sellerservice.checkStatus().subscribe((respose)=>{
+       this.applicationStatus1 = respose;
+    });
+    } 
 
   onLogout() {
     this.cookieservice.deleteAll();
